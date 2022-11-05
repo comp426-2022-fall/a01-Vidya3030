e86b965773d78720491f3584679bc457cce8de4a
Vidya3030 <vidyar3030@gmail.com>
2022-11-05 19:11:15 -0400

    Finished first function

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ var https = require('https');
 // Require fs module
 var fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
-var parseArgs = require('minimist');
+var args = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-
+arg1 = minimist(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-
+const port = arg1 || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-
+let serverfunction = (a,b) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-
-
-
-
-
+try {
+    file = fs.readFile('./public/index.html');
+}
+catch(err) {console.log(err)}
+}
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
