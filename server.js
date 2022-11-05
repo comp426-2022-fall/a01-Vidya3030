030da814a1a9871204030b480a59ddc406b54ae0
Vidya3030 <vidyar3030@gmail.com>
2022-11-05 19:45:15 -0400

    Fixed some issues (hopeflly)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
 var args = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-arg1 = minimist(process.argv.slice(2));
+var arg1 = minimist(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = arg1 || 3000;
@@ -13,12 +13,12 @@ const port = arg1 || 3000;
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-let serverfunction = () => {
+fs.readFile('./public/index.html', (err, data) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-    try {file = fs.readFile('./public/index.html');}
-    catch(err) {console.log(err)}
-}
+    if (err) {console.error(err);}
+    else var file = data;
+});
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
@@ -27,12 +27,12 @@ let serverfunction = () => {
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req, res) {
     res.statusCode(200);
-    res.setHeader('text/html');
+    res.setHeader('Content-Type', 'text/html');
     res.end(file);
-})
+});
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen(port) => {
+server.listen(port, () => {
     console.log('Server listening on the port ${port}')
-}
+});
 // That's it! You're all done!
