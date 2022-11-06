b470105ee2b54381ed8bd9d2712dce28ddaad1ce
Vidya3030 <vidyar3030@gmail.com>
2022-11-05 20:33:24 -0400

    Changed back to file

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ const port = args.port || 3000;
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
+var file;
 fs.readFile('./public/index.html', (err, data) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
     if (err) {console.error(err); return;}
-    //else var file = data;
+    file = data;
 });
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -27,7 +28,7 @@ fs.readFile('./public/index.html', (err, data) => {
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end(data);
+    res.end(file);
 });
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
