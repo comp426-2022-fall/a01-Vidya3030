8fbb77736c4897ebb3b5aa30dfd6595201a394c8
Vidya3030 <vidyar3030@gmail.com>
2022-11-05 20:51:48 -0400

    Added node_modules to gitignore

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ var file;
 fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-    if (err) {console.error(err); return;}
+    if (err) {
+        console.error(err); 
+        return;
+    }
     file = data;
 });
 // Define a const `server` as an arrow function using http.createServer. 
