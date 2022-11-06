b6c07b77589382a4ccee522c808ccae489e00b3e
Vidya3030 <vidyar3030@gmail.com>
2022-11-05 20:13:22 -0400

    Tried more for empty server error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const port = args.port || 3000;
 fs.readFile('./public/index.html', (err, data) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-    if (err) {console.error(err);}
+    if (err) {console.error(err); return;}
     else var file = data;
 });
 // Define a const `server` as an arrow function using http.createServer. 
